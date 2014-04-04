aabfd8fa3c8766c525d47dd9c1e8db3df8509202
Benjamin Singer <benjaminsinger@Benjamins-MacBook-Pro-3.local>
2014-04-03 19:46:49 -0500

    validations

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,15 @@
 class Character < ActiveRecord::Base
   attr_accessible :storm_of_swords, :feast_for_crows, :breaking_the_law, :bolton_knows_best, :best_served_cold, :in_littlefingers_brothel, :you_win_or_you_die, :name
 
+validates :name, :presence => true
+validates :storm_of_swords, numericality: { only_integer: true }
+validates :feast_for_crows, numericality: { only_integer: true }
+validates :breaking_the_law, numericality: { only_integer: true }
+validates :bolton_knows_best, numericality: { only_integer: true }
+validates :best_served_cold, numericality: { only_integer: true }
+validates :in_littlefingers_brothel, numericality: { only_integer: true }
+validates :you_win_or_you_die, numericality: { only_integer: true }
+
 def total_points
   self.storm_of_swords + self.feast_for_crows + self.breaking_the_law + self.bolton_knows_best + self.best_served_cold + self.in_littlefingers_brothel + self.you_win_or_you_die 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :c1, :c2, :c3, :c4, :c5
 
+  validates :name, :presence => true
+  validates :c1, numericality: { only_integer: true }
+  validates :c2, numericality: { only_integer: true }
+  validates :c3, numericality: { only_integer: true }
+  validates :c4, numericality: { only_integer: true }
+  validates :c5, numericality: { only_integer: true }
+
   def total_points
   @c1 = Character.find(self.c1)
   @c2 = Character.find(self.c2)
